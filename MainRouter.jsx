1d97d2272764b5a39ad74b8276f47c018cde33f8
Hardik <hardilpandya20@gmail.com>
2025-02-04 23:32:44 +0500

    deployemtn fix

--- MainRouter.jsx
+++ MainRouter.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import Home from './components/Home'
+import Home from './src/Home'
 import About from './src/about'
 import Contact from './src/contact'
 import Education from './src/education'
